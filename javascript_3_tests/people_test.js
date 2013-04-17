b4d938aee630a32d47774564b614c9cd6b1e89c0
rauschma <axel@rauschma.de>
2013-04-17 15:56:27 +0200

    Dateien verschoben, mehr Tests für die Hausaufgabe.

--- javascript_3_tests/people_test.js
+++ javascript_3_tests/people_test.js
@@ -1,8 +1,18 @@
 var assert = require('assert');
-var people = require('../../js7_code/javascript_2_code/people');
+var people = require('../../js7_ha/javascript_3_ha/people');
+var Person = people.Person;
+var Employee = people.Employee;
 
-var john = new people.Person('John');
+var john = new Person('John');
 assert.equal(john.describe(), 'Person called John');
+assert.equal(john.name, 'John');
+assert.ok(john instanceof Person);
+assert.ok(john.describe === Person.prototype.describe);
 
-var jane = new people.Employee('Jane', 'CTO');
+var jane = new Employee('Jane', 'CTO');
 assert.equal(jane.describe(), 'Person called Jane (CTO)');
+assert.equal(jane.name, 'Jane');
+assert.equal(jane.title, 'CTO');
+assert.ok(jane instanceof Person);
+assert.ok(jane instanceof Employee);
+assert.ok(jane.describe === Employee.prototype.describe);
