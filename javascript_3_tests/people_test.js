e16ecca5e5646c3c633dca7b5d846bb1ecbd486c
rauschma <axel@rauschma.de>
2013-04-18 12:00:51 +0200

    Tests für die Struktur der Konstruktoren.

--- javascript_3_tests/people_test.js
+++ javascript_3_tests/people_test.js
@@ -3,12 +3,19 @@ var people = require('../../js7_ha/javascript_3_ha/people');
 var Person = people.Person;
 var Employee = people.Employee;
 
+// Struktur der Konstruktoren
+assert.ok(Person.prototype.isPrototypeOf(Employee.prototype));
+assert.ok(Person.prototype.constructor === Person);
+assert.ok(Employee.prototype.constructor === Employee);
+
+// Eine Instanz von Person
 var john = new Person('John');
 assert.equal(john.describe(), 'Person called John');
 assert.equal(john.name, 'John');
 assert.ok(john instanceof Person);
 assert.ok(john.describe === Person.prototype.describe);
 
+// Eine Instanz von Employee
 var jane = new Employee('Jane', 'CTO');
 assert.equal(jane.describe(), 'Person called Jane (CTO)');
 assert.equal(jane.name, 'Jane');
